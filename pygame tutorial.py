6b50e51766a3cca670e3511d5cfd8889a8cd3cd6
mafe2233 <mafe2233@gmai.com>
2016-05-03 11:40:38 -0300

    Criei os 5 botoes e adicionei eles alinhados na guitarra

--- pygame tutorial.py
+++ pygame tutorial.py
@@ -25,6 +25,15 @@ bot1Img = pygame.transform.scale(bot1Img, (100,100))
 bot2Img = pygame.image.load('botao2.png')
 bot2Img = pygame.transform.scale(bot2Img, (100,100))
 
+bot3Img = pygame.image.load('botao3.png')
+bot3Img = pygame.transform.scale(bot3Img, (100,100))
+
+bot4Img = pygame.image.load('botao4.png')
+bot4Img = pygame.transform.scale(bot4Img, (100,100))
+
+bot5Img = pygame.image.load('botao5.png')
+bot5Img = pygame.transform.scale(bot5Img, (100,100))
+
 guitarraImg = pygame.image.load('guitarra.png')
 guitarraImg = pygame.transform.scale(guitarraImg, (800,600))
 
@@ -32,9 +41,16 @@ def bot1(x, y):
     gameDisplay.blit(bot1Img,(x,y))
 
 def bot2(x, y):
-    gameDisplay.blit(bot2Img,( x+79 ,y-100))
+    gameDisplay.blit(bot2Img,( x+79 , y-100))
+ 
+def bot3(x, y):
+    gameDisplay.blit(bot3Img,(x+158 , y-50))
     
+def bot4(x, y):
+    gameDisplay.blit(bot4Img,(x+236 , y+20))
 
+def bot5(x, y):
+    gameDisplay.blit(bot5Img,(x+316 , y+100))
 
 x = (190)
 
@@ -69,7 +85,10 @@ while not crashed:
     gameDisplay.blit(guitarraImg, (0, 0))
     bot1(x, y)
     bot2(x, y)
-
+    bot3(x, y)    
+    bot4(x, y)
+    bot5(x, y)
+    
     pygame.display.update()
     clock.tick(60)
     
