bf2bd9632506a10849067e5aac01b5316436530e
elisamalzoni <elisammfb@gmail.com>
2016-05-03 11:21:40 -0300

    adicionei guitarra e alinhei os buttons

--- pygame tutorial.py
+++ pygame tutorial.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -8,7 +9,7 @@ display_height = 600
 
 black = (0,0,0)
 white = (255,255,255)
-
+purple = (151,65,239)
 
 
 
@@ -18,20 +19,25 @@ clock = pygame.time.Clock()
 crashed = False
 
 bot1Img = pygame.image.load('botao1.png')
-bot1Img = pygame.transform.scale(bot1Img, (50,100))
+bot1Img = pygame.transform.scale(bot1Img, (100,100))
 
 
 bot2Img = pygame.image.load('botao2.png')
-bot2Img = pygame.transform.scale(bot2Img, (50,100))
+bot2Img = pygame.transform.scale(bot2Img, (100,100))
 
+guitarraImg = pygame.image.load('guitarra.png')
+guitarraImg = pygame.transform.scale(guitarraImg, (800,600))
 
 def bot1(x, y):
     gameDisplay.blit(bot1Img,(x,y))
 
 def bot2(x, y):
-    gameDisplay.blit(bot2Img,( x+50 ,y-100))
+    gameDisplay.blit(bot2Img,( x+79 ,y-100))
+    
 
-x = (display_width/2)
+
+x = (190)
+
 y = (display_height*0.2)
 y_change = 0
 
@@ -59,7 +65,8 @@ while not crashed:
     y += y_change
     
     
-    gameDisplay.fill(white)
+    gameDisplay.fill(purple)
+    gameDisplay.blit(guitarraImg, (0, 0))
     bot1(x, y)
     bot2(x, y)
 
