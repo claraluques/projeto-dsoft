45daf722204e3ea201d8f3fbb4d8d92f7c3b650f
elisamalzoni <elisammfb@gmail.com>
2016-05-04 09:35:25 -0300

    voltei, sem classes agora

--- Bolinha.py
+++ Bolinha.py
@@ -3,28 +3,38 @@ import pygame
 class Bolinha:
     def __init__(self, y):
         self.y = y
-        
-    def bolinha_posicao1(self):
-        self.bolinha1Img = pygame.image.load('botao1.png')
-        self.bolinha1Img = pygame.transform.scale(self.bolinha1Img, (100,100))
-        self.gameDisplay.blit(self.bolinha1Img,(190,y))
-        
-    def bolinha_posicao2(self, y):
-        self.bolinha2Img = pygame.image.load('botao2.png')
-        self.bolinha2Img = pygame.transform.scale(self.bolinha2Img, (100,100))
-        self.gameDisplay.blit(self.bolinha2Img,(269,y))
-        
-    def bolinha_posicao3(self, y):
-        self.bolinha3Img = pygame.image.load('botao3.png')
-        self.bolinha3Img = pygame.transform.scale(self.bolinha3Img, (100,100))
-        self.gameDisplay.blit(self.bolinha3Img,(348,y))
-        
-    def bolinha_posicao4(self, y):
-        self.bolinha4Img = pygame.image.load('botao4.png')
-        self.bolinha4Img = pygame.transform.scale(self.bolinha4Img, (100,100))
-        self.gameDisplay.blit(self.bolinha4Img,(427,y))
-        
-    def bolinha_posicao5(self, y):
-        self.bolinha5Img = pygame.image.load('botao5.png')
-        self.bolinha5Img = pygame.transform.scale(self.bolinha5Img, (100,100))
-        self.gameDisplay.blit(self.bolinha5Img,(506,y))
+
+    
+    def posx(self, n, y):
+        display_width = 800
+        display_height = 600
+        gameDisplay = pygame.display.set_mode((display_width, display_height))        
+
+        x = 190
+        if n == 0:
+            x = x
+            self.bolinha1Img = pygame.image.load('botao1.png')
+            self.bolinha1Img = pygame.transform.scale(self.bolinha1Img, (100,100))
+            gameDisplay.blit(self.bolinha1Img,(x,y))            
+        elif n >= 1:
+            x += 79*n
+            if n == 1:
+                self.bolinha2Img = pygame.image.load('botao2.png')
+                self.bolinha2Img = pygame.transform.scale(self.bolinha2Img, (100,100))
+                gameDisplay.blit(self.bolinha2Img,(x,y))
+            elif n == 2:             
+                self.bolinha3Img = pygame.image.load('botao3.png')
+                self.bolinha3Img = pygame.transform.scale(self.bolinha3Img, (100,100))
+                gameDisplay.blit(self.bolinha3Img,(348,y))               
+            elif n == 3:
+                self.bolinha4Img = pygame.image.load('botao4.png')
+                self.bolinha4Img = pygame.transform.scale(self.bolinha4Img, (100,100))
+                gameDisplay.blit(self.bolinha4Img,(427,y))
+            elif n == 4:
+                self.bolinha5Img = pygame.image.load('botao5.png')
+                self.bolinha5Img = pygame.transform.scale(self.bolinha5Img, (100,100))
+                gameDisplay.blit(self.bolinha5Img,(506,y))               
+
+
+            
+  
--- pygame tutorial.py
+++ pygame tutorial.py
@@ -1,5 +1,5 @@
 import pygame
-import Bolinha
+from Bolinha import Bolinha
 
 pygame.init()
 
@@ -7,7 +7,6 @@ display_width = 800
 display_height = 600
 
 
-black = (0,0,0)
 white = (255,255,255)
 purple = (151,65,239)
 
@@ -17,45 +16,49 @@ clock = pygame.time.Clock()
 crashed = False
 
 
-#
-#bot1Img = pygame.image.load('botao1.png')
-#bot1Img = pygame.transform.scale(bot1Img, (100,100))
-#
-#bot2Img = pygame.image.load('botao2.png')
-#bot2Img = pygame.transform.scale(bot2Img, (100,100))
-#
-#bot3Img = pygame.image.load('botao3.png')
-#bot3Img = pygame.transform.scale(bot3Img, (100,100))
-#
-#bot4Img = pygame.image.load('botao4.png')
-#bot4Img = pygame.transform.scale(bot4Img, (100,100))
-#
-#bot5Img = pygame.image.load('botao5.png')
-#bot5Img = pygame.transform.scale(bot5Img, (100,100))
-#
 guitarraImg = pygame.image.load('guitarra.png')
 guitarraImg = pygame.transform.scale(guitarraImg, (800,600))
 
-#def bot1(x, y):
-#    gameDisplay.blit(bot1Img,(x,y))
-#
-#def bot2(x, y):
-#    gameDisplay.blit(bot2Img,( x+79 , y-100))
-# 
-#def bot3(x, y):
-#    gameDisplay.blit(bot3Img,(x+158 , y-50))
-#    
-#def bot4(x, y):
-#    gameDisplay.blit(bot4Img,(x+236 , y+20))
-#
-#def bot5(x, y):
-#    gameDisplay.blit(bot5Img,(x+316 , y+100))
-#
-#x = (190)
+
+bot1Img = pygame.image.load('botao1.png')
+bot1Img = pygame.transform.scale(bot1Img, (100,100))
+
+bot2Img = pygame.image.load('botao2.png')
+bot2Img = pygame.transform.scale(bot2Img, (100,100))
+
+bot3Img = pygame.image.load('botao3.png')
+bot3Img = pygame.transform.scale(bot3Img, (100,100))
+
+bot4Img = pygame.image.load('botao4.png')
+bot4Img = pygame.transform.scale(bot4Img, (100,100))
+
+bot5Img = pygame.image.load('botao5.png')
+bot5Img = pygame.transform.scale(bot5Img, (100,100))
+
+
+
+def bot1(x, y):
+    gameDisplay.blit(bot1Img,(x,y))
+
+def bot2(x, y):
+    gameDisplay.blit(bot2Img,( x+79 , y-100))
+ 
+def bot3(x, y):
+    gameDisplay.blit(bot3Img,(x+158 , y-50))
+    
+def bot4(x, y):
+    gameDisplay.blit(bot4Img,(x+236 , y+20))
+
+def bot5(x, y):
+    gameDisplay.blit(bot5Img,(x+316 , y+100))
+
+x = (190)
 
 y = (display_height*0.2)
 y_change = 0
 
+xacerto1 = display_height*0.8
+xacerto2 = display_height
 
 while not crashed:
     for event in pygame.event.get():
@@ -67,6 +70,8 @@ while not crashed:
                 y_change = 5
             elif event.key == pygame.K_UP:
                 y_change = -5
+            elif event.key == pygame.K_q:
+                if 
             
             
         if event.type == pygame.KEYUP:
@@ -75,9 +80,9 @@ while not crashed:
             elif event.key == pygame.K_UP:
                 y_change = 0
     
-        
-    bolinha1 = Bolinha(y)
-    bolinha1.bolinha_posicao1()
+    
+            
+
         
     y += y_change
     
@@ -86,11 +91,16 @@ while not crashed:
     gameDisplay.fill(purple)
     gameDisplay.blit(guitarraImg, (0, 0))
     
-#    bot1(x, y)
-#    bot2(x, y)
-#    bot3(x, y)    
-#    bot4(x, y)
-#    bot5(x, y)
+
+#    bolinha1 = Bolinha(y)
+#    bolinha1.posx(0,y)
+
+        
+    bot1(x, y)
+    bot2(x, y)
+    bot3(x, y)    
+    bot4(x, y)
+    bot5(x, y)
     
     pygame.display.update()
     clock.tick(60)
