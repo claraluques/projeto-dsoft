95ca8884cf6f9b56fecc3b85549f38ba63f79a95
Maria Clara Luques <mariacll@al.insper.edu.br>
2016-05-06 09:10:39 -0300

    ele está lendo a lista das notas, porém nao consigo fazer os botoes aparecerem. eba.

--- classe_musicas.py
+++ classe_musicas.py
@@ -3,6 +3,9 @@ class musicas:
         oimusica1 = open('doremifa.txt', 'r')
         musica1txt = oimusica1.readlines()
         oimusica1.close()
-        musica1 = list(musica1txt[0])
+        musica1str = list(musica1txt[0])
+        musica1 = []
+        for i in range (len(musica1str)):
+            musica1.append(int(musica1str[i]))
         return musica1
         
