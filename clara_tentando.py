14e3dfed18127422066b4e213f0a023aa6e09e7e
Maria Clara Luques <mariacll@al.insper.edu.br>
2016-05-18 11:12:26 -0300

    codigo alterado para que possamos ter duas bolinhas ao mesmo tempo

--- clara_tentando.py
+++ clara_tentando.py
@@ -2,9 +2,38 @@ import pygame
 import classe_musicas as m
 import classe_teclas as t
 
+#def process_key(y, score):
+#    d1 = y - 550
+#    if d1 < 0:
+#        d1 = -d1
+#    if d1 < 5:
+#        print('PERFECT')
+#        Perfect(650,100)
+##        y = random.randrange(-600,0)
+#        score +=3
+#        print("SCORE: {0}".format(score))
+#        
+#    elif d1 < 15:
+#        print ("VERY GOOD")
+#        score += 2
+#        print("SCORE: {0}".format(score))
+##        y =random.randrange(-600,0)
+#    elif d1 < 25:
+#        print ("GOOD")
+#        score += 1
+#        print("SCORE: {0}".format(score))
+##        y = random.randrange(-600,0)                    
+#    elif d1 < 100:
+#        m.erro()
+#        print("MISSED")
+##   elif y > 550: 
+#        
+#    return(score)
+
 pygame.init()
 
 pygame.mixer.init(44100, -16,2,2048)
+#pygame.mixer.music.load(m.audio1())
 
 display_width = 800
 display_height = 650
@@ -31,11 +60,48 @@ guitarraImg = pygame.transform.scale(guitarraImg, (800,650))
 
 pause = False
 
-modo = -1 
+modo = 0 #0: modo normal 1: modo aleatorio
+
+#botoes = []
+#for i in range(5):
+#    filename = 'botao{0}.png'.format(i+1)
+#    botaoImg = pygame.image.load(filename)
+#    botaoImg = pygame.transform.scale(botaoImg, (100,100))
+#    
+#    botoes.append(botaoImg)
 
 PerfectImg = pygame.image.load('Perfect.sprite.png')
 PerfectImg = pygame.transform.scale(PerfectImg, (100,100))
    
+#bot1Img = pygame.image.load('botao1.png')
+#bot1Img = pygame.transform.scale(bot1Img, (100,100))
+#
+#bot2Img = pygame.image.load('botao2.png')
+#bot2Img = pygame.transform.scale(bot2Img, (100,100))
+#
+#bot3Img = pygame.image.load('botao3.png')
+#bot3Img = pygame.transform.scale(bot3Img, (100,100))
+#
+#bot4Img = pygame.image.load('botao4.png')
+#bot4Img = pygame.transform.scale(bot4Img, (100,100))
+#
+#bot5Img = pygame.image.load('botao5.png')
+#bot5Img = pygame.transform.scale(bot5Img, (100,100))
+#
+#def bot1(x, y1):
+#    gameDisplay.blit(bot1Img,(x,y1))
+#
+#def bot2(x, y2):
+#    gameDisplay.blit(bot2Img,( x+79 , y2))
+# 
+#def bot3(x, y3):
+#    gameDisplay.blit(bot3Img,(x+158 , y3))
+#    
+#def bot4(x, y4):
+#    gameDisplay.blit(bot4Img,(x+236 , y4))
+#
+#def bot5(x, y5):
+#    gameDisplay.blit(bot5Img,(x+316 , y5))
 def bot(x,y,Img):
     botImg = pygame.image.load(Img)
     botImg = pygame.transform.scale(botImg, (100,100))
@@ -45,13 +111,16 @@ def Score(count):
     font = pygame.font.SysFont(None,40)
     text = font.render("Score: "+str(count), True, black)
     gameDisplay.blit(text,(650,0))    
+    
 
 def Perfect(x, y):
     gameDisplay.blit(PerfectImg ,(650, 200))
+    
 
 def text_objects(text, font):
     textSurface = font.render(text, True, black)
     return textSurface, textSurface.get_rect()
+    
 
 def button(msg,x,y,w,h,ic,ac,action):
     mouse = pygame.mouse.get_pos()
@@ -74,6 +143,7 @@ def sair_jogo():
     global intro, escolha
     escolha = "Sair"
     intro = False
+    
 
 def sair_jogo2():
     pygame.quit()
@@ -120,15 +190,19 @@ def game_intro():
                 pygame.quit()
                 quit()
         
+      
+        
         gameDisplay.fill(white)
         largeText = pygame.font.Font('freesansbold.ttf',100)
         TextSurf, TextRect = text_objects("Guitar Student", largeText)
         TextRect.center = ((display_width/2),(display_height/2))
         gameDisplay.blit(TextSurf, TextRect)
         
+#        button("GO!",150,450,100,50,green,bright_green,loop_jogo)
         button("Quit",550,450,100,50,red,bright_red,sair_jogo)
         button("Baile",150,550,100,50,green,bright_green,escolha_modo1)        
         button("Aleatoria",450,550,100,50,green,bright_green,escolha_modo3)
+        
         
         pygame.display.update()
         clock.tick(15)
@@ -138,6 +212,7 @@ def game_intro():
     else:
         sair_jogo2()
         
+
 def unpause():
         global pause
         pause = False
@@ -165,30 +240,44 @@ def paused():
         pygame.display.update()
         clock.tick(15)
 
+
 def loop_jogo():
     global intro, escolha
     escolha = "Go"
     intro = False
     
+    
 def loop_jogo2():    
     ganhou = False
 
-    musica = m.musica(modo)
-    listay1 = musica[0]
-    listay2 = musica[1]
-    listay3 = musica[2]
-    listay4 = musica[3]
-    listay5 = musica[4]
+    musica1 = m.musica(modo)
+    listay1 = musica1[0]
+    listay2 = musica1[1]
+    listay3 = musica1[2]
+    listay4 = musica1[3]
+    listay5 = musica1[4]
+
         
     x = (190)
-
+    
+    #y1 =random.randrange(-600,0)
+    #y2 =random.randrange(-600,0)
+    #y3 =random.randrange(-600,0)
+    #y4 =random.randrange(-600,0)
+    #y5 =random.randrange(-600,0)
+    
     y_change = 5
+    
+    #xacerto1 = display_height*0.8
+    #xacerto2 = display_height    
     
     score = 0
     
     frames= 0
     
     crashed = False    
+    
+#    videointro.stop()
     
     while not crashed:
         global pause        
@@ -212,6 +301,7 @@ def loop_jogo2():
             listay1[i] += y_change
             if y1 < miny:
                 miny = y1
+            
                 
         for i in range (len(listay2)):
             y2 = listay2[i]
@@ -242,12 +332,13 @@ def loop_jogo2():
                 miny = y5
         
         if miny >= display_height:
-            musica = m.musica(modo)
-            listay1 = musica[0]
-            listay2 = musica[1]
-            listay3 = musica[2]
-            listay4 = musica[3]
-            listay5 = musica[4]
+            musica1 = m.musica(modo)
+            listay1 = musica1[0]
+            listay2 = musica1[1]
+            listay3 = musica1[2]
+            listay4 = musica1[3]
+            listay5 = musica1[4]
+
      
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -267,17 +358,17 @@ def loop_jogo2():
                 elif event.key == pygame.K_e:
                     for i in range (len(listay3)):
                         y3 = listay3[i]
-                        score = t.process_key3(y3, score)
+                        score = t.process_key(y3, score)
                         
                 elif event.key == pygame.K_r:
                     for i in range (len(listay4)):
                         y4 = listay4[i]
-                        score = t.process_key4(y4, score)
+                        score = process_key(y4, score)
                                             
                 elif event.key == pygame.K_t:
                     for i in range (len(listay5)):
                         y5 = listay5[i]
-                        score = t.process_key5(y5, score)
+                        score = process_key(y5, score)
                         
                 elif event.key == pygame.K_ESCAPE:
                     pause = True
@@ -287,6 +378,28 @@ def loop_jogo2():
                 if event.key == pygame.K_DOWN or event.key == pygame.K_UP:
                     y_change = 0
     
+    #    bot1(x, y1)
+    #    bot2(x, y2)
+    #    bot3(x, y3)    
+    #    bot4(x, y4)
+    #    bot5(x, y5)
+        
+    #    if y1 > display_height:
+    #        y1 = random.randrange(-200,0) 
+    #        
+    #    if y2 > display_height:
+    #        y2 = random.randrange(-100,0)
+    #        
+    #    if y3 > display_height:
+    #        y3 = random.randrange(-400,0)
+    #        
+    #    if y4 > display_height:
+    #        y4 = random.randrange(-250,0)
+    #        
+    #    if y5 > display_height:
+    #        y5 = random.randrange(-300,0)
+        
+        
         if score == 100:
             ganhou = True
         if ganhou == True:
--- classe_musicas.py
+++ classe_musicas.py
@@ -11,29 +11,42 @@ def musica1():
     pygame.mixer.music.play(0)
     pygame.mixer.music.set_volume(0.2)
     
-    oimusica1 = open('baile.txt', 'r')
-    musica1txt = oimusica1.readlines()
-    oimusica1.close()
-    musica1str = list(musica1txt[0])
-    musica1 = []
-    for i in range (len(musica1str)):
-        musica1.append(int(musica1str[i]))
+    oimusica = open('baile.txt', 'r')
+    musicatxt = oimusica.readlines()
+    oimusica.close()
+    musicastr = list(musicatxt[0])
+    musica = []
+    for i in range (len(musicastr)):
+        musica.append(int(musicastr[i]))
     y1=[]
     y2=[]
     y3=[]
     y4=[]
     y5=[]
-    for i in range (len(musica1)):
-        if musica1[i] == 0:
+    for i in range (len(musica)):
+        if musica[i] == 0:
             y1.append(-100*i)
-        elif musica1[i] == 1:
+        elif musica[i] == 1:
             y2.append(-100*i)
-        elif musica1[i] == 2:
+        elif musica[i] == 2:
             y3.append(-100*i)
-        elif musica1[i] == 3:
+        elif musica[i] == 3:
             y4.append(-100*i)
-        elif musica1[i] == 4:
+        elif musica[i] == 4:
             y5.append(-100*i)
+        elif musica[i] == 5:
+            y1.append(-100*i)
+            y3.append(-100*i)
+        elif musica[i] == 6:
+            y2.append(-100*i)
+            y4.append(-100*i)
+        elif musica[i] == 7:
+            y4.append(-100*i)
+            y3.append(-100*i)
+            y5.append(-100*i)
+        elif musica[i] == 9:
+            y1.append(-100*i)
+            y2.append(-100*i)
     return y1, y2, y3, y4, y5
     
     
@@ -42,32 +55,42 @@ def musica2():
     pygame.mixer.music.play(0)
     pygame.mixer.music.set_volume(1)
 
-    oimusica2 = open('cliffs_of_dover.txt', 'r')
-    musica2txt = oimusica2.readlines()
-    oimusica2.close()
-    musica2str = list(musica2txt[0])
-    musica2 = []
-    for i in range (len(musica2str)):
-        musica2.append(int(musica2str[i]))
+    oimusica = open('cliffs_of_dover.txt', 'r')
+    musicatxt = oimusica.readlines()
+    oimusica.close()
+    musicastr = list(musicatxt[0])
+    musica = []
+    for i in range (len(musicastr)):
+        musica.append(int(musicastr[i]))
     y1=[]
     y2=[]
     y3=[]
     y4=[]
     y5=[]
-    for i in range (len(musica2)):
-        if musica2[i] == 0:
+    for i in range (len(musica)):
+        if musica[i] == 0:
             y1.append(-100*i)
-        elif musica2[i] == 1:
+        elif musica[i] == 1:
             y2.append(-100*i)
-        elif musica2[i] == 2:
+        elif musica[i] == 2:
             y3.append(-100*i)
-        elif musica2[i] == 3:
+        elif musica[i] == 3:
             y4.append(-100*i)
-        elif musica2[i] == 4:
+        elif musica[i] == 4:
             y5.append(-100*i)
-        elif musica2[i] == 5:
+        elif musica[i] == 5:
             y1.append(-100*i)
             y3.append(-100*i)
+        elif musica[i] == 6:
+            y2.append(-100*i)
+            y4.append(-100*i)
+        elif musica[i] == 7:
+            y4.append(-100*i)
+            y3.append(-100*i)
+            y5.append(-100*i)
+        elif musica[i] == 9:
+            y1.append(-100*i)
+            y2.append(-100*i)
             
     return y1, y2, y3, y4, y5
     
